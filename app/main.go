429417fbf72d4025d98fc2364fadb37231b4fc89
fesenko_darya <darya.dobrafesenko@mail.ru>
2021-05-31 13:10:50 +0300

    init commit

---
+++ app/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/DaryaFesenko/go_2_les_3/app/fibonacci"
+)
+
+func main() {
+	f := fibonacci.Fibonacci(3)
+
+	fmt.Println(f)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/DaryaFesenko/go_2_les_3
+
+go 1.16
